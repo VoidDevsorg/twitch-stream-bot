901fff8f5e5ae6e772f9f8a44ac01847fb46b103
Maya <foenx0315@gmail.com>
2023-06-05 13:43:05 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,8 @@
 const config = {
   client: {
-    id: "your_client_id",
-    secret: "your_client_secret",
-    token: "your_client_token",
+    id: "your_discord_bot_client_id",
+    secret: "your_discord_bot_client_secret",
+    token: "your_discord_bot_token",
   },
 };
 
